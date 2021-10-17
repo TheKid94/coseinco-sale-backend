6423a82789b923afe16f7928f828da17ece0c246
Gustavo <gustavopoth@gmail.com>
2021-10-16 22:36:06 -0500

    Reservado finalizado con estado

--- src/controllers/GuiaController.js
+++ src/controllers/GuiaController.js
@@ -1,4 +1,6 @@
 const Guia = require("../models/Guia");
+const Inventario = require("../models/Inventario");
+const Pedido = require("../models/Pedido");
 
 const getGuiabyId = async (req, res) => {
 
@@ -14,17 +16,49 @@ const createGuia = async(req, res) => {
     let productos = req.body.productos;
     let nseries = [];
     let guia = new Object();
-    for(var i=0;i<productos.length;i++){
-        nseries.push(productos[i]);
+    try{
+        for(var i=0;i<productos.length;i++){
+            let instock = await Inventario.findOne({productoID: productos[i].productoID});
+            let myArray = transformArray(instock.nSerie);
+            let toRemove = transformArray(productos[i].serialNumbers);
+            let difference = transformDifference(myArray,toRemove);
+            await Inventario.findOneAndUpdate({productoID: productos[i].productoID},
+                {"stock":instock.stock - productos[i].serialNumbers.length,"nSerie":difference}
+            )
+            nseries.push(productos[i]);
+        };
+        await Pedido.findOneAndUpdate({codigoPedido:pedidoscod},{"estado":"reservado"});
+        guia.codigoPedido = pedidoscod;
+        guia.nseries = nseries;
+        let guiares = await Guia.create(guia);
+        res.status(200).json({
+            guiares
+        })
+    }catch(error){
+        res.status(500).json({
+            error
+        })
     }
-    guia.pedidoID = pedidoscod;
-    guia.nseries = nseries;
-    let guiares = await Guia.create(guia);
-    res.status(200).json({
-        guiares
-    })
 }
 
+const transformArray = (arr) => {
+    let array = [];
+    for(let i=0;i < arr.length;i++){
+        array.push(arr[i]);
+    }
+    return array;
+}
+
+const transformDifference = (myArray, toRemove)=>{
+    for(let i=0;i<myArray.length;i++){
+        for(let j=0; j<toRemove.length;j++){
+            if(myArray[i] === toRemove[j]){
+                myArray.splice(i,1);
+            }
+        }
+    }
+    return myArray;
+}
 module.exports = {
     getGuiabyId,
     createGuia
--- src/models/Guia.js
+++ src/models/Guia.js
@@ -1,7 +1,7 @@
 const { Schema, model } = require('mongoose');
 
 const Guia = new Schema({
-    pedidoID: String,
+    codigoPedido: String,
     nseries: Array,
     empleadoID: {
         type: String, 
