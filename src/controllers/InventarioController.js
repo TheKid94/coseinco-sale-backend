b32f87c43fb6622e2e55390bae029e3a9152fdb0
Gustavo <gustavopoth@gmail.com>
2021-10-19 20:51:49 -0500

    Inventarios get all

--- src/controllers/InventarioController.js
+++ src/controllers/InventarioController.js
@@ -1,5 +1,6 @@
 const Inventario = require('../models/Inventario');
 const Producto = require('../models/Producto');
+const Marca = require('../models/Marca');
 
 const getSeriesByProductId = async(req,res) => {
     let id = req.params.id;
@@ -21,6 +22,34 @@ const getSeriesByProductId = async(req,res) => {
     }   
 }
 
+const getInventarios = async(req,res)=>{
+    let inventarioslist = [];
+    try{
+        let inventarios = await Inventario.find({});
+        for(var i=0; i<inventarios.length;i++){
+            let inventarioaux = new Object();
+            let producto = await Producto.findById(inventarios[i].productoID);
+            let marca = await Marca.findById(producto.marcaID);
+            inventarioaux.productoID = producto._id;
+            inventarioaux.sku = producto.SKU;
+            inventarioaux.imagen = producto.imagenes[0];
+            inventarioaux.nombre = producto.nombre;
+            inventarioaux.cantidad = inventarios[i].stock;
+            inventarioaux.marca = marca.nombre;
+            inventarioslist.push(inventarioaux);
+        }
+        res.status(200).json({
+            status: 'success',
+            inventarioslist
+        })
+    }catch(err){
+        return res.status(500).json({
+            error: err
+        })
+    }
+}
+
 module.exports ={
-    getSeriesByProductId
+    getSeriesByProductId,
+    getInventarios
 }
--- src/routes/InventarioRoutes.js
+++ src/routes/InventarioRoutes.js
@@ -3,5 +3,6 @@ const router = Router();
 const inventarioController = require('../controllers/InventarioController');
 
 router.route('/getSeriesByProductId/:id').get(inventarioController.getSeriesByProductId);
+router.route('/getInventarios').get(inventarioController.getInventarios);
 
 module.exports = router
