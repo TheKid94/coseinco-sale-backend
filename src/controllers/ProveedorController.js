d13aa6050ee3da005d3976a30a1a6a15e3db5d07
Maciel Gonzales <leicam10@outlook.com>
2021-11-14 21:59:05 -0500

    modificar proveedor

--- src/controllers/ProveedorController.js
+++ src/controllers/ProveedorController.js
@@ -1,101 +1,120 @@
-const Proveedor = require('../models/Proveedor');
+const Proveedor = require("../models/Proveedor");
 
 const getAll = (req, res) => {
-    Proveedor.find({},(err, proveedores) =>{
-        if(err){
-            return res.status(500).json({
-                message: `Error al realizar la peticion: ${err}`
-            })
-        }
+  Proveedor.find({}, (err, proveedores) => {
+    if (err) {
+      return res.status(500).json({
+        message: `Error al realizar la peticion: ${err}`,
+      });
+    }
 
-        if(!proveedores){
-            return res.status(404).json({
-                message: 'No existen los proveedores'
-            })
-        }
-        res.status(200).json({
-            status: 'success',
-            proveedores
-        });
+    if (!proveedores) {
+      return res.status(404).json({
+        message: "No existen los proveedores",
+      });
+    }
+    res.status(200).json({
+      status: "success",
+      proveedores,
     });
-}
+  });
+};
 
 const getOne = (req, res) => {
-
-    const id = req.params.id;
-    Proveedor.findById(id,(err,proveedor)=>{
-        if(err){
-            return res.status(500).json({
-                message: `Error al realizar la peticion ${err}`
-            })
-        }
-        if(!proveedor){
-            return res.status(404).json({
-                message: 'No existe la proveedor'
-            })
-        }
-        res.status(200).json({
-            status: 'success',
-            proveedor
-        });
+  const id = req.params.id;
+  Proveedor.findById(id, (err, proveedor) => {
+    if (err) {
+      return res.status(500).json({
+        message: `Error al realizar la peticion ${err}`,
+      });
+    }
+    if (!proveedor) {
+      return res.status(404).json({
+        message: "No existe la proveedor",
+      });
+    }
+    res.status(200).json({
+      status: "success",
+      proveedor,
     });
-
+  });
 };
 
 const deshabilitarProveedor = async (req, res) => {
-    let id = req.body.id;
-    try{
-        await Proveedor.updateOne({_id: id},{estado:"deshabilitado"});
-        res.status(200).json({
-            status: 'success'
-        })
-    }catch(err){
-        res.status(500).json({
-            error:err
-        })
-    }
+  let id = req.body.id;
+  try {
+    await Proveedor.updateOne({ _id: id }, { estado: "deshabilitado" });
+    res.status(200).json({
+      status: "success",
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: err,
+    });
+  }
 };
 
 const habilitarProveedor = async (req, res) => {
-    let id = req.body.id;
-    try{
-        await Proveedor.updateOne({_id: id},{estado:"habilitado"});
-        res.status(200).json({
-            status: 'success'
-        })
-    }catch(err){
-        res.status(500).json({
-            error:err
-        })
-    }
+  let id = req.body.id;
+  try {
+    await Proveedor.updateOne({ _id: id }, { estado: "habilitado" });
+    res.status(200).json({
+      status: "success",
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: err,
+    });
+  }
 };
 
-const createProveedor = async(req,res) =>{
-    try{
-        const proveedor = req.body.proveedor;
-        if(Object.keys(proveedor).length == 0){
-            return res.status(400).json({
-                status: 'warning',
-                status: 'Debe ingresar un proveedor correcto'
-            });
-        }
-        const proveres = await Proveedor.create(proveedor)
-        res.status(200).json({
-            status: 'success',
-            proveres
-        })
-    }catch(error){
-        res.status(500).json({
-            error
-        })
+const createProveedor = async (req, res) => {
+  try {
+    const proveedor = req.body.proveedor;
+    if (Object.keys(proveedor).length == 0) {
+      return res.status(400).json({
+        status: "warning",
+        status: "Debe ingresar un proveedor correcto",
+      });
     }
-}
+    const proveres = await Proveedor.create(proveedor);
+    res.status(200).json({
+      status: "success",
+      proveres,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error,
+    });
+  }
+};
 
+const modifyProveedor = async (req, res) => {
+  try {
+    const proveedor = req.body.proveedor;
+    const id = req.body._id;
+    await Proveedor.findOneAndUpdate(
+      { _id : id},
+      {
+        razonSocial:proveedor.razonSocial,
+        ruc: proveedor.ruc,
+        correo: proveedor.correo,
+        contacto: proveedor.contacto,
+        telefono: proveedor.telefono,
+        descuento: proveedor.descuento,
+      }
+    );
+    res.json({ msg: "Proveedor actualizado" });
+  } catch (error) {
+   res.status(500).json({error});
+  }
+};
 
 module.exports = {
-    getAll,
-    getOne,
-    createProveedor,
-    deshabilitarProveedor,
-    habilitarProveedor
-}
+  getAll,
+  getOne,
+  createProveedor,
+  modifyProveedor,
+  deshabilitarProveedor,
+  habilitarProveedor,
+};
--- src/routes/ProveedorRoutes.js
+++ src/routes/ProveedorRoutes.js
@@ -6,8 +6,9 @@ router.route('/').get(proveedorController.getAll);
 router.route('/getProveedor/:id').get(proveedorController.getOne);
 
 router.route('/create').post(proveedorController.createProveedor);
+router.route('/modify').post(proveedorController.modifyProveedor);
 router.route('/deshabilitar').post(proveedorController.deshabilitarProveedor);
 router.route('/habilitar').post(proveedorController.habilitarProveedor);
-
+    
 
 module.exports = router;
