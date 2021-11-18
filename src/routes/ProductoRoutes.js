99aef70fa4ecce6b3d9c4a7082a388292f3d34ed
Gustavo <gustavopoth@gmail.com>
2021-11-17 21:56:31 -0500

    Actualizar OCompra

--- src/routes/ProductoRoutes.js
+++ src/routes/ProductoRoutes.js
@@ -7,6 +7,7 @@ router.route('/getById/:id').get(productoController.getOne);
 router.route('/ProductosCompra').get(productoController.getAllProductoCompra);
 router.route('/productoCarrito').post(productoController.productoCarrito);
 router.route('/productoCreate').post(productoController.createProducto);
+router.route('/productoUpdate').post(productoController.updateProducto);
 router.route('/productoImagen').post(productoController.ImagenProductoURL);
 router.route('/anular').post(productoController.inhabilitarProducto);
 router.route('/habilitar').post(productoController.habilitarProducto);
