6cbf84cb10e8c1b6038dc5981e52a4bf8d103ed3
Gustavo <gustavopoth@gmail.com>
2024-08-30 15:57:55 -0500

    Configurando endpoint de chatbot

--- src/controllers/ChatBotController.js
+++ src/controllers/ChatBotController.js
@@ -1,11 +1,15 @@
 const Pedido = require("../models/Pedido");
+const DetallePedido = require("../models/DetallePedido");
+const Producto = require("../models/Producto");
+const Inventario = require("../models/Inventario");
 
 const getPedidoByNumberDoc = async(req,res)=>{
     let numDoc = req.body.numeroDoc;
     let numPedido = req.body.numeroPedido;
     try
     {
-        let pedido = await Pedido.findOne({ codigoPedido: numPedido, 'datos.numberDoc': numDoc}); 
+        let pedido = await Pedido.findOne({ codigoPedido: numPedido, 'datos.numberDoc': numDoc});
+        let detallePedido = await DetallePedido.findOne({ pedidoID: pedido._id}); 
         if(!pedido)
         {
             return res.status(404).json({
@@ -15,7 +19,60 @@ const getPedidoByNumberDoc = async(req,res)=>{
 
         res.status(200).json({
             status: 'success',
-            pedido
+            pedido,
+            productos: detallePedido.productos
+        })
+    }
+    catch(error)
+    {
+        res.status(500).json({
+            error
+        })
+    }
+   
+}
+
+const getProductoBySKU = async(req,res)=>{
+    let sku = req.body.sku;
+    try
+    {
+        let producto = await Producto.findOne({ SKU: sku});
+        if(!producto)
+        {
+            return res.status(404).json({
+                message: 'No existe el producto o el numero de sku es invalido al producto que busca'
+            })
+        }
+
+        res.status(200).json({
+            status: 'success',
+            producto
+        })
+    }
+    catch(error)
+    {
+        res.status(500).json({
+            error
+        })
+    }
+   
+}
+
+const getProductosByCategoriaId = async(req,res)=>{
+    let categoriaId = req.body.categoriaId;
+    try
+    {
+        let productos = await Producto.find({ categoriaID: categoriaId}); 
+        if(productos.length == 0)
+        {
+            return res.status(404).json({
+                message: 'No hay productos'
+            })
+        }
+
+        res.status(200).json({
+            status: 'success',
+            producto
         })
     }
     catch(error)
@@ -28,5 +85,6 @@ const getPedidoByNumberDoc = async(req,res)=>{
 }
 
 module.exports = {
-    getPedidoByNumberDoc
+    getPedidoByNumberDoc,
+    getProductoBySKU
   };
--- src/routes/ChatBotRoutes.js
+++ src/routes/ChatBotRoutes.js
@@ -2,6 +2,7 @@ const { Router } = require( 'express' );
 const router = Router();
 const chatBotController = require('../controllers/ChatBotController')
 
-router.route('/pedidoByCliente').get(chatBotController.getPedidoByNumberDoc);
+router.route('/pedidoByCliente').post(chatBotController.getPedidoByNumberDoc);
+router.route('/productoById').get(chatBotController.getProductoBySKU);
 
 module.exports = router;
