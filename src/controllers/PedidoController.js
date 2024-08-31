f196731d517ec7b7f58b9f5e061373690bd4c14a
Gustavo <gustavopoth@gmail.com>
2024-08-31 09:15:12 -0500

    añadiendo pago de pedido

--- src/controllers/PedidoController.js
+++ src/controllers/PedidoController.js
@@ -9,6 +9,12 @@ const Usuario = require("../models/Usuario");
 const Inventario = require("../models/Inventario");
 const Envio = require("../models/Envio");
 
+const fs = require('fs');
+const path = require('path');
+const utils = require('util');
+const puppeteer = require('puppeteer');
+const hb = require('handlebars');
+const readFile = utils.promisify(fs.readFile);
 const cloudinary = require('cloudinary');
 
 cloudinary.config({
@@ -63,57 +69,136 @@ const getOne = (req, res) => {
 
 const createPedido = async (req, res) => {
   try {
-    let productos = req.body.productos;
-    let datos = req.body.datos;
-    let nPedidos = await Pedido.find({});
-    let newPedido = new Object();
-    let newDetallePedido = new Object();
-    let items = [];
-    let total = 0.0;
-    if (productos.length == 0 || Object.keys(datos).length == 0) {
-      res.status(400).json({
-        status: "error",
-      });
-      return false;
+    const { productos, datos } = req.body;
+    
+    // Validación de entrada
+    if (!productos || productos.length === 0 || !datos || Object.keys(datos).length === 0) {
+      return res.status(400).json({ status: "error", message: "Datos de entrada inválidos" });
     }
-    newPedido.codigoPedido = "P" + `${nPedidos.length + 1}`.padStart(5, "0");
-    newPedido.fechaRegistro = new Date();
-    newPedido.fechaEntrega = new Date(Date.now() + 2*24*3600*1000)
-    for (var i = 0; i < productos.length; i++) {
-      let producto = await Producto.findById(productos[i]._id);
-      let marcatemp = await Marca.findById(producto.marcaID);
-      let itemproducto = new Object();
-      itemproducto.productoID = productos[i]._id,
-      itemproducto.nombre = producto.nombre;
-      itemproducto.marca = marcatemp.nombre;
-      itemproducto.SKU = producto.SKU;
-      itemproducto.cantidad = parseInt(productos[i].cantidad),
-      itemproducto.preciounitario = producto.precio;
-      itemproducto.subtotal = productos[i].cantidad * producto.precio;
-      itemproducto.imagen = producto.imagenes[0];
+
+    const nPedidos = await Pedido.countDocuments();
+    
+    // Inicialización del pedido
+    const newPedido = {
+      codigoPedido: `P${(nPedidos + 1).toString().padStart(5, "0")}`,
+      fechaRegistro: new Date(),
+      fechaEntrega: new Date(Date.now() + 2 * 24 * 3600 * 1000), // 2 días después
+      precioVenta: 0,
+      observacion: "No hay observaciones",
+      estado: "generado",
+      datos: datos,
+      files: []
+    };
+
+    const items = [];
+    let total = 0;
+
+    // Obtención de detalles del producto y cálculo del total
+    for (const item of productos) {
+      const producto = await Producto.findById(item._id).populate('marcaID', 'nombre');
+      if (!producto) {
+        return res.status(404).json({ status: "error", message: `Producto no encontrado: ${item._id}` });
+      }
+
+      const itemproducto = {
+        productoID: producto._id,
+        nombre: producto.nombre,
+        marca: producto.marcaID.nombre,
+        SKU: producto.SKU,
+        cantidad: parseInt(item.cantidad),
+        preciounitario: producto.precio,
+        subtotal: parseInt(item.cantidad) * producto.precio,
+        imagen: producto.imagenes[0]
+      };
+
       total += itemproducto.subtotal;
       items.push(itemproducto);
     }
+
     newPedido.precioVenta = total;
-    newPedido.observacion = "No hay observaciones";
-    newPedido.estado = "generado";
-    newPedido.datos = datos;
-    newPedido.files = [];
+
+    // Creación del pedido y detalle
     const pedido = await Pedido.create(newPedido);
-    newDetallePedido.pedidoID = pedido._id;
-    newDetallePedido.productos = items;
-    newDetallePedido.totalPrecio = total;
-    const dePedido = await DetallePedido.create(newDetallePedido);
-    const productosped = dePedido.productos;
+    const newDetallePedido = {
+      pedidoID: pedido._id,
+      productos: items,
+      totalPrecio: total
+    };
+    const detallePedido = await DetallePedido.create(newDetallePedido);
+
+    // Generación de archivo PDF y envío por correo
+    await generarFilePago(pedido, detallePedido);
+
     res.status(200).json({
       status: "success",
       pedido,
-      productosped,
+      productos: items
     });
   } catch (error) {
-    res.status(500).json({
-      error,
+    console.error("Error al crear el pedido:", error);
+    res.status(500).json({ status: "error", message: error.message });
+  }
+};
+
+const generarFilePago = async (pedido, detallePedido) => {
+  try {
+    const resp = await getTemplatePagoHtml();
+    let reshtml = resp;
+    let prod = "";
+
+    const fechaReg = pedido.fechaRegistro;
+    const fechaEnt = pedido.fechaEntrega;
+
+    reshtml = reshtml.replace('#CodigoVenta', pedido.codigoPedido);
+    reshtml = reshtml.replace('#FechaRegistro', `${fechaReg.getDate() + 1}/${fechaReg.getMonth() + 1}/${fechaReg.getFullYear()}`);
+    reshtml = reshtml.replace('#FechaLlegada', `${fechaEnt.getDate() + 1}/${fechaEnt.getMonth() + 1}/${fechaEnt.getFullYear()}`);
+    reshtml = reshtml.replace('#Cliente', `${pedido.datos.name} ${pedido.datos.lastName}`);
+    reshtml = reshtml.replace('#Correo', pedido.datos.email);
+    reshtml = reshtml.replace('#TipoDocumento', pedido.datos.documentType);
+    reshtml = reshtml.replace('#NumDocumento', pedido.datos.numberDoc);
+
+    detallePedido.productos.forEach(producto => {
+      prod += `<tr class="item"><td>${producto.nombre}</td><td>${producto.SKU}</td><td>${producto.cantidad}</td><td>${producto.subtotal}</td></tr>`;
     });
+
+    reshtml = reshtml.replace('#Productos', prod);
+    reshtml = reshtml.replace('#Total', `<strong style="float: right; margin-right: 45px;">Total: ${detallePedido.totalPrecio}</strong>`);
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setContent(reshtml);
+    const pathfile = `src/public/${pedido.codigoPedido}.pdf`;
+    await page.pdf({ path: pathfile, format: 'A4' });
+
+    const resultcloud = await cloudinary.uploader.upload(pathfile, { folder: `Coseinco/Ventas/Boleta/${pedido.codigoPedido}` });
+    await Pedido.findOneAndUpdate({ _id: pedido._id }, { $push: { files: resultcloud.url } });
+
+    const pdfContent = fs.readFileSync(pathfile).toString('base64');
+
+    await client.sendEmail({
+      From: "gustavo.troncos@urp.edu.pe",
+      To: pedido.datos.email,
+      Subject: `Generación de Boleta de Venta ${pedido.codigoPedido}`,
+      HtmlBody: reshtml,
+      MessageStream: "broadcast",
+      Attachments: [
+        {
+          Name: `${pedido.codigoPedido}.pdf`,
+          Content: pdfContent,
+          ContentType: 'application/pdf'
+        }
+      ]
+    });
+
+    fs.unlink(pathfile, (err) => {
+      if (err) {
+        console.error("Error al eliminar el archivo PDF:", err);
+      }
+    });
+
+  } catch (err) {
+    console.error("Error en generarFilePago:", err);
+    throw new Error(err.message);
   }
 };
 
@@ -430,99 +515,100 @@ async function getTemplatePagoHtml() {
   
 }
 
-const generarPagoDoc = async(req, res)=>{
-  let npedido = req.body.npedido;
-  try{
-      let pedido = await Pedido.findOne({ codigoPedido: npedido });
-      let detallePedido = await DetallePedido.findOne({ pedidoID: pedido._id });
-      let dataMail = {};
+// const generarPagoDoc = async(req, res)=>{
+//   let npedido = req.body.npedido;
+//   try{
+//       let pedido = await Pedido.findOne({ codigoPedido: npedido });
+//       let detallePedido = await DetallePedido.findOne({ pedidoID: pedido._id });
+//       let dataMail = {};
 
-      let resp = await getTemplatePagoHtml();
-      let prod = "";
-      let reshtml = resp;
+//       let resp = await getTemplatePagoHtml();
+//       let prod = "";
+//       let reshtml = resp;
 
-      let fechaReg = pedido.fechaRegistro;
-      let diaReg = fechaReg.getDate();
-      let mesReg = fechaReg.getMonth();
-      let anioReg = fechaReg.getFullYear();
+//       let fechaReg = pedido.fechaRegistro;
+//       let diaReg = fechaReg.getDate();
+//       let mesReg = fechaReg.getMonth();
+//       let anioReg = fechaReg.getFullYear();
 
-      let fechaEnt = pedido.fechaEntrega;
-      let diaEnt = fechaEnt.getDate();
-      let mesEnt = fechaEnt.getMonth();
-      let anioEnt = fechaEnt.getFullYear();
+//       let fechaEnt = pedido.fechaEntrega;
+//       let diaEnt = fechaEnt.getDate();
+//       let mesEnt = fechaEnt.getMonth();
+//       let anioEnt = fechaEnt.getFullYear();
 
-      const total = '<strong style="float: right; margin-right: 45px;">' + 'Total: ' + detallePedido.totalPrecio + '</strong> ';
+//       const total = '<strong style="float: right; margin-right: 45px;">' + 'Total: ' + detallePedido.totalPrecio + '</strong> ';
 
-      reshtml = reshtml.replace('#CodigoVenta', pedido.codigoPedido);
-      reshtml = reshtml.replace('#FechaRegistro', `${diaReg + 1}/${mesReg + 1}/${anioReg}`);
-      reshtml = reshtml.replace('#FechaLlegada', `${diaEnt + 1}/${mesEnt + 1}/${anioEnt}`);
-      reshtml = reshtml.replace('#Cliente', `${pedido.datos.name} ${pedido.datos.lastName}`);
-      reshtml = reshtml.replace('#Correo', `${pedido.datos.email}`);
-      reshtml = reshtml.replace('#TipoDocumento', `${pedido.datos.documentType}`);
-      reshtml = reshtml.replace('#NumDocumento', `${pedido.datos.numberDoc}`);
+//       reshtml = reshtml.replace('#CodigoVenta', pedido.codigoPedido);
+//       reshtml = reshtml.replace('#FechaRegistro', `${diaReg + 1}/${mesReg + 1}/${anioReg}`);
+//       reshtml = reshtml.replace('#FechaLlegada', `${diaEnt + 1}/${mesEnt + 1}/${anioEnt}`);
+//       reshtml = reshtml.replace('#Cliente', `${pedido.datos.name} ${pedido.datos.lastName}`);
+//       reshtml = reshtml.replace('#Correo', `${pedido.datos.email}`);
+//       reshtml = reshtml.replace('#TipoDocumento', `${pedido.datos.documentType}`);
+//       reshtml = reshtml.replace('#NumDocumento', `${pedido.datos.numberDoc}`);
 
-      for (var i = 0; i < detallePedido.productos.length; i++) {
-          prod += '<tr class="item">';
-          prod += `<td>${detallePedido.productos[i].nombre}</td>`;
-          prod += `<td style="text-align: left !important;">${detallePedido.productos[i].SKU}</td>`;
-          prod += `<td>${detallePedido.productos[i].cantidad}</td>`;
-          prod += `<td>${detallePedido.productos[i].subtotal}</td>`;
-          prod += '</tr>';
-      }
+//       for (var i = 0; i < detallePedido.productos.length; i++) {
+//           prod += '<tr class="item">';
+//           prod += `<td>${detallePedido.productos[i].nombre}</td>`;
+//           prod += `<td style="text-align: left !important;">${detallePedido.productos[i].SKU}</td>`;
+//           prod += `<td>${detallePedido.productos[i].cantidad}</td>`;
+//           prod += `<td>${detallePedido.productos[i].subtotal}</td>`;
+//           prod += '</tr>';
+//       }
 
-      reshtml = reshtml.replace('#Productos', prod);
-      reshtml = reshtml.replace('#Total', total);
+//       reshtml = reshtml.replace('#Productos', prod);
+//       reshtml = reshtml.replace('#Total', total);
 
-      const template = hb.compile(reshtml, { strict: true });
-      let result = template(dataMail);
-      let html = result;
+//       const template = hb.compile(reshtml, { strict: true });
+//       let result = template(dataMail);
+//       let html = result;
 
-      const browser = await puppeteer.launch();
-      const page = await browser.newPage();
-      await page.setContent(html);
+//       const browser = await puppeteer.launch();
+//       const page = await browser.newPage();
+//       await page.setContent(html);
 
-      let pathfile = `src/public/${pedido.codigoPedido}.pdf`;
-      await page.pdf({ path: pathfile, format: 'A4' });
+//       let pathfile = `src/public/${pedido.codigoPedido}.pdf`;
+//       await page.pdf({ path: pathfile, format: 'A4' });
 
-      const resultcloud = await cloudinary.uploader.upload(pathfile, { folder: `Coseinco/Ventas/Boleta/${pedido.codigoPedido}` });
-      await browser.close();
+//       const resultcloud = await cloudinary.uploader.upload(pathfile, { folder: `Coseinco/Ventas/Boleta/${pedido.codigoPedido}` });
+//       await browser.close();
 
-      fs.unlink(pathfile, (err) => {
-          if (err) {
-              res.status(500).json({ error: err });
-          }
-      });
+//       fs.unlink(pathfile, (err) => {
+//           if (err) {
+//               res.status(500).json({ error: err });
+//           }
+//       });
 
-      const pdfContent = fs.readFileSync(pathfile).toString('base64');
-      await Pedido.findOneAndUpdate({ codigoPedido: pedido.codigoPedido }, { $push: { files: resultcloud.url } });
+//       const pdfContent = fs.readFileSync(pathfile).toString('base64');
+//       await Pedido.findOneAndUpdate({ codigoPedido: pedido.codigoPedido }, { $push: { files: resultcloud.url } });
 
-      client.sendEmail({
-          "From": "gustavo.troncos@urp.edu.pe",
-          "To": pedido.datos.email,
-          "Subject": `Generación de Boleta de Venta ${pedido.codigoPedido}`,
-          "HtmlBody": reshtml,
-          "MessageStream": "broadcast",
-          "Attachments": [
-              {
-                  "Name": `${pedido.codigoPedido}.pdf`,
-                  "Content": pdfContent,
-                  "ContentType": 'application/pdf'
-              }
-          ]
-        }, (error, result) => {
-          if (error) {
-              res.status(500).json({ error: error.message });
-          } else {
-              res.status(200).json({ status: 'success', message: 'Email sent successfully!' });
-          }
-      });
+//       client.sendEmail({
+//           "From": "gustavo.troncos@urp.edu.pe",
+//           "To": pedido.datos.email,
+//           "Subject": `Generación de Boleta de Venta ${pedido.codigoPedido}`,
+//           "HtmlBody": reshtml,
+//           "MessageStream": "broadcast",
+//           "Attachments": [
+//               {
+//                   "Name": `${pedido.codigoPedido}.pdf`,
+//                   "Content": pdfContent,
+//                   "ContentType": 'application/pdf'
+//               }
+//           ]
+//         }, (error, result) => {
+//           if (error) {
+//               res.status(500).json({ error: error.message });
+//           } else {
+//               res.status(200).json({ status: 'success', message: 'Email sent successfully!' });
+//           }
+//       });
 
-  }catch(err){
-      res.status(500).json({
-          error: err.message
-      });
-  }
-}
+//   }catch(err){
+//       res.status(500).json({
+//           error: err.message
+//       });
+//   }
+// }
+
 
 
 module.exports = {
