5e599fd9a80175a1f512d4b7d2205cefa4cee2ac
Gustavo <gustavopoth@gmail.com>
2024-11-19 01:58:56 -0500

    subiendo modificaciones en Usuario

--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -1,13 +1,24 @@
 const {Schema , model} = require('mongoose');
 
-const Usuario = new Schema({
+const DatosSchema = new Schema({
+    nombres: String,
+    apellidos: String,
+    telefono: String,
+    tipoDocumento: String,
+    numeroDocumento: String,
+    correo: String,
+    fechaNacimiento: Date,
+    direccion:String
+}, { _id: false });
 
+const Usuario = new Schema({
     nombreUsuario : String, 
     rolID : String, 
     password : String, 
-    datos : Object   
+    datos : DatosSchema   
 },{
     versionKey: false
 });
+
 
 module.exports = model('Usuario', Usuario, 'Usuario');  
